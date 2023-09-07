cfbd2b08c86e7c50adbdf6b00dfd1de5212dacc1
axel <axosar@gmail.com>
2023-09-07 15:16:27 -0600

    title

--- src/app/categorias/[categoria]/page.tsx
+++ src/app/categorias/[categoria]/page.tsx
@@ -12,6 +12,16 @@ export default function Categoria() {
             <Logo/>
             <p className='font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
             <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase mb-5'>{titulo}</h2>
+            {titulo === 'COLOCACION' && (
+                <>
+                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">311-07</button>
+                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">301-05</button>
+                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">302 IR-04</button>
+                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">304-00</button>
+                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">305 R-99</button>
+                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">306 88</button>
+                </>
+            )}
         </main>
     )
 }
