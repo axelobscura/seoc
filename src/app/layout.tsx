998b5f9de10806bbd027e27acaf769d4a3e3da6b
axel <axosar@gmail.com>
2023-09-07 13:45:43 -0600

    title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;500;1000&display=swap" rel="stylesheet"></link>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className='text-4xl text-bold text-uppercase text-gray-500'>Supervisor Especializado en Obras de Concreto</h1>
+      <h1 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase'>Supervisor Especializado en Obras de Concreto</h1>
     </main>
   )
 }
