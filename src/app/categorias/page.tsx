4d2f740879b8f32d0af25141a69b7bd9abc20ab2
axel <axosar@gmail.com>
2023-09-07 14:34:37 -0600

    title

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -7,7 +7,7 @@ export default function Categorias() {
         'Supervisión',
         'Control de calidad',
         'Pruebas',
-        'Prácticas estándar o recomendaciones',
+        'Prácticas estándar',
         'Materiales',
         'Mezclas de concreto',
         'Mezclado de concreto',
@@ -37,11 +37,13 @@ export default function Categorias() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <Logo/>
-            <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase'>CATEGORÍAS</h2>
             <p className='font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
-            {categorias.map((val) => (
-                <button key={val} className="rounded-full bg-blue-950 px-32 py-2 mt-3 font-sans font-extralight w-full">{val}</button>
-            ))}
+            <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase mb-5'>CATEGORÍAS</h2>
+            <div className="grid grid-cols-3 gap-4">
+                {categorias.map((val) => (
+                    <button key={val} className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">{val}</button>
+                ))}
+            </div>
         </main>
     )
 }
