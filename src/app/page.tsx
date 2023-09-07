189c5ef862ab2750d214420abb8c032bacbffe08
axel <axosar@gmail.com>
2023-09-07 14:37:06 -0600

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <Logo/>
-      <h1 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase'>Supervisor Especializado en Obras de Concreto</h1>
       <p className='font-sans text-gray-500 font-bold'><small>Instituto Mexicano del Cemento y del Concreto A.C.</small></p>
+      <h1 className='text-4xl md:text-2md text-bold text-uppercase text-gray-500 font-sans font-extralight text-center uppercase'>Supervisor Especializado en Obras de Concreto</h1>
       <Link href="/categorias">
         <button className="rounded-full bg-blue-950 px-32 py-2 mt-5 font-sans font-extralight">CATEGORÍAS</button>
       </Link>
