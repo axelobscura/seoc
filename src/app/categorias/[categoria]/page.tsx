ac3f48614a8d916ea53f258f45e9cf4a7f597045
axel <axosar@gmail.com>
2023-09-07 15:36:10 -0600

    title

--- src/app/categorias/[categoria]/page.tsx
+++ src/app/categorias/[categoria]/page.tsx
@@ -24,7 +24,10 @@ export default function Categoria() {
             <p className='font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
             <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase mb-5'>{titulo}</h2>
             {titulo === 'COLOCACION' && normas.map(norma => (
-                <button key={norma} className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">{norma}</button>
+                <Link className='w-full' key={norma} href={`/categorias/${tit}/${norma.split(' ').join('-')}`}>
+                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">{norma}</button>
+                </Link>
+                
             ))}
         </main>
     )
