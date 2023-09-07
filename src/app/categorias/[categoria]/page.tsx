e877282951b0c7025b1f749e3492c1c1b747b28b
axel <axosar@gmail.com>
2023-09-07 15:30:05 -0600

    title

--- src/app/categorias/[categoria]/page.tsx
+++ src/app/categorias/[categoria]/page.tsx
@@ -1,9 +1,20 @@
 "use client"
 import { useParams } from 'next/navigation'
+import Link from 'next/link'
 import { useState } from 'react'
 import Logo from '../../components/Logo'
 
 export default function Categoria() {
+    const [normas, setNormas] = useState([
+        '311-07',
+        '301-05',
+        '302 IR-04',
+        '304-00',
+        '305 R-99',
+        '306 88'
+    ])
+
+
     const params = useParams()
     let tit = params.categoria;
     let titulo = (tit as string).split('-').join(' ').toUpperCase()
@@ -12,16 +23,9 @@ export default function Categoria() {
             <Logo/>
             <p className='font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
             <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase mb-5'>{titulo}</h2>
-            {titulo === 'COLOCACION' && (
-                <>
-                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">311-07</button>
-                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">301-05</button>
-                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">302 IR-04</button>
-                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">304-00</button>
-                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">305 R-99</button>
-                    <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">306 88</button>
-                </>
-            )}
+            {titulo === 'COLOCACION' && normas.map(norma => (
+                <button key={norma} className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-3 font-sans font-extralight w-full uppercase">{norma}</button>
+            ))}
         </main>
     )
 }
