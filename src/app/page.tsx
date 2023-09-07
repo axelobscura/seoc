5eb0f907f4749112d7e5cfec7bff3478306d9e61
axel <axosar@gmail.com>
2023-09-07 13:29:24 -0600

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className='text-4xl text-gray-500 text-bold text-uppercase'>Supervisor Expecializado en Obras de Concreto</h1>
+      <h1 className='text-4xl text-bold text-uppercase text-gray-500'>Supervisor Especializado en Obras de Concreto</h1>
     </main>
   )
 }
