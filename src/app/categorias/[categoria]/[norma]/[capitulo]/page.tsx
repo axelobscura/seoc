25231ee4cd6a74c48cb3e973c362c9864634a0a9
axel <axosar@gmail.com>
2023-09-07 16:06:30 -0600

    title

--- src/app/categorias/[categoria]/[norma]/[capitulo]/page.tsx
+++ src/app/categorias/[categoria]/[norma]/[capitulo]/page.tsx
@@ -6,12 +6,14 @@ import Logo from '../../../../components/Logo'
 
 export default function Capitulo() {
     const params = useParams()
-    console.log(params)
+    let cap: string | any = params.capitulo
+    let capi = cap.split('-').join(' ')
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <Logo/>
             <p className='font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
-            <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase mb-5'>CAPÍTULOS {params.norma}</h2>
+            <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase mb-5'>NORMA {params.norma}</h2>
+            <h2 className='text-2xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase mb-5'>{capi}</h2>
         </main>
     )
 }
