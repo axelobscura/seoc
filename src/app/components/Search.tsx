a6b572e66acfdd766a13f4952686897c83065731
axel <axosar@gmail.com>
2023-09-14 14:31:23 -0600

    search-component

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/solid'
 
 export default function Search() {
 
-  
+
 
   const buscar = (e: any) => {
     console.log(e)
@@ -16,7 +16,7 @@ export default function Search() {
             <span className="sr-only">Search</span>
             <input className="placeholder:italic placeholder:text-slate-400 text-gray-900 block bg-white w-1/2 border border-slate-300 rounded-full py-5 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Buscar por contenidos..." type="text" name="search"/>
             <span className="inset-y-0 left-0 flex items-center pl-2" onClick={buscar}>
-                <MagnifyingGlassCircleIcon className="h-14 w-14 text-gray-300 hover:text-slate-400 cursor-pointer"/>
+                <MagnifyingGlassCircleIcon className="h-16 w-16 text-gray-300 hover:text-slate-400 cursor-pointer"/>
             </span>
         </label>
     </div>
