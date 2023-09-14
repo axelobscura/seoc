d120679f40b7a6ede687ebfa2123b47b2dcaeac1
axel <axosar@gmail.com>
2023-09-14 14:29:26 -0600

    search-component

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -40,7 +40,7 @@ export default function Categorias() {
             <p className='font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
             <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans font-extralight uppercase mb-5'>CATEGORÍAS</h2>
             <Search/>
-            <div className="grid grid-cols-3 gap-4">
+            <div className="grid grid-cols-3 gap-4 mt-5">
                 {categorias.map((val) => (
                     <Link key={val} href={`/categorias/${val.split(' ').join('-').toLowerCase()}`}>
                         <button className="font-sans rounded-full bg-blue-950 px-5 py-2 mt-1 font-sans font-extralight w-full uppercase text-white">{val}</button>
--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,21 +1,24 @@
 "use client";
+import { useRef } from 'react';
 import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/solid'
 
 export default function Search() {
 
-    const buscar = (e: any) => {
-      console.log(e)
-    }
+  
 
-    return(
-      <div className='my-2 w-full'>
-          <label className="flex items-center justify-center relative block">
-              <span className="sr-only">Search</span>
-              <input className="placeholder:italic placeholder:text-slate-400 text-gray-900 block bg-white w-1/2 border border-slate-300 rounded-full py-5 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Buscar por contenidos..." type="text" name="search"/>
-              <span className="inset-y-0 left-0 flex items-center pl-2" onClick={buscar}>
-                  <MagnifyingGlassCircleIcon className="h-14 w-14 text-gray-300 hover:text-slate-400 cursor-pointer"/>
-              </span>
-          </label>
-      </div>
-    )
+  const buscar = (e: any) => {
+    console.log(e)
+  }
+
+  return(
+    <div className='my-2 w-full'>
+        <label className="flex items-center justify-center relative block">
+            <span className="sr-only">Search</span>
+            <input className="placeholder:italic placeholder:text-slate-400 text-gray-900 block bg-white w-1/2 border border-slate-300 rounded-full py-5 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Buscar por contenidos..." type="text" name="search"/>
+            <span className="inset-y-0 left-0 flex items-center pl-2" onClick={buscar}>
+                <MagnifyingGlassCircleIcon className="h-14 w-14 text-gray-300 hover:text-slate-400 cursor-pointer"/>
+            </span>
+        </label>
+    </div>
+  )
 }
