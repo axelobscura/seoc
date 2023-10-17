55c558d8710ce2e8c389bf2a7e48a74a18ce6163
axel <axosar@gmail.com>
2023-10-16 19:11:43 -0600

    entrada

---
+++ src/app/login/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+export default function Login() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24" style={{
+      background: 'url(bkg_login.jpg) no-repeat center center fixed',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+    }}>
+      <div className='flex flex-col login' style={{
+        'marginLeft': '55%'
+      }}>
+        <form className='mx-5 mt-3 fixed bottom-10 left-1/3 w-1/4'>
+          <div className="mb-1">
+            <label className="block font-medium text-gray-900">Correo electrónico:</label>
+            <input className="block w-full px-4 py-1 border border-gray-300 bg-gray-900 text-white rounded-full focus:outline-none focus:border-blue-500 text-gray-900" type="email" id="email" name="email" />
+          </div>
+          <div className="mb-2">
+            <label className="block font-medium text-gray-900">Contraseña:</label>
+            <input className="block w-full px-4 py-1 border border-gray-300 bg-gray-900 text-white rounded-full focus:outline-none focus:border-blue-500 text-gray-900" type="password" id="password" name="password" />
+          </div>
+          <button className="bg-black text-white rounded-full py-2 px-4 w-full mt-2" type="submit">INGRESAR</button>
+        </form>
+      </div>
+    </main>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,6 @@ import Search from './components/Search'
 
 
 export default function Home() {
-
-
-
-  const registro = () => {
-    console.log('registro')
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24" style={{
       background: 'url(bkg_entrada.jpg) no-repeat center center fixed',
@@ -21,33 +14,19 @@ export default function Home() {
       backgroundPosition: 'center center',
       backgroundSize: 'contain',
     }}>
-      <div className='login' style={{
+      <div className='flex items-center flex-col login' style={{
         'marginLeft': '55%'
       }}>
         <Image
           src='/logo_seoc_entrada.png'
-          width='300'
-          height='300'
+          width='400'
+          height='400'
           alt="seoc"
         />
-        <form className='mx-5 mt-3'>
-          <div className="mb-2">
-            <label className="block font-medium text-gray-500">Correo electrónico:</label>
-            <input className="block w-full px-4 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-gray-900" type="email" id="email" name="email" />
-          </div>
-          <div className="mb-2">
-            <label className="block font-medium text-gray-500">Contraseña:</label>
-            <input className="block w-full px-4 py-1 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-gray-900" type="password" id="password" name="password" />
-          </div>
-          <button className="bg-black text-white rounded-full py-2 px-4 w-full mt-5" type="submit">INGRESAR</button>
-        </form>
+        <Link href='/login'>
+          <button className="bg-black text-white rounded-full py-2 px-20 mt-5 mr-7 ml-7" type="submit">INGRESAR</button>
+        </Link>
       </div>
-      {/* 
-      <Search/>
-      <Link href="/categorias">
-        <button className="rounded-full bg-blue-950 px-32 py-2 mt-5 font-sans font-extralight text-white">CATEGORÍAS</button>
-      </Link>
-      */}
     </main>
   )
 }
