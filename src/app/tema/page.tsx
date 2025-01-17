a13feb024bf52ffee1d6d91ff7e398efb4c2e498
axelinstrumental <aobscura@instrumental.net>
2025-01-17 14:54:47 -0600

    implementation-for-edit

--- src/app/tema/page.tsx
+++ src/app/tema/page.tsx
@@ -1,16 +1,30 @@
+"use client"
+import Link from 'next/link'
 import Logo from '../components/Logo'
 import Search from '../components/Search'
+import { useCategorias } from '../../../lib/swr-hooks'
+import Loader from '@/app/components/Loader'
 
 export default function Tema() {
-    
+    const {categorias, isLoadingCategorias} = useCategorias();
+    if(isLoadingCategorias){
+        return(
+            <Loader />
+        )
+    };
+    console.log(categorias);
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <Logo/>
-            <p className='font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
+            <p className='text-smooch font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
             <h2 className='text-4xl text-bold text-uppercase text-gray-500 font-sans uppercase mb-5'>CATEGORÍAS</h2>
             <Search/>
             <div className="grid grid-cols-3 gap-4 mt-5">
-              <h2>Tema</h2>
+                {categorias.map((val: any) => (
+                    <Link key={val.id} href={`/categorias/${val.url}?id=${val.id}`}>
+                        <button className="text-smooch rounded-full bg-sky-900 px-5 py-2 mt-1 w-full uppercase text-white hover:bg-sky-950">{val.nombre}</button>
+                    </Link>
+                ))}
             </div>
         </main>
     )
--- src/app/tipo-de-busqueda/page.tsx
+++ src/app/tipo-de-busqueda/page.tsx
@@ -1,16 +1,15 @@
 import Link from "next/link"
 
 export default function TipoDeBusqueda() {
-    
     return (
         <main className="grid grid-cols-2 min-h-screen flex-col items-center justify-center p-0">
           <Link href={{ pathname: '/tema' }}>
-            <div className="flex min-h-screen bg-black hover:bg-white items-center justify-center">
+            <div className="flex min-h-screen bg-black hover:bg-gray-700 items-center justify-center">
               <h2 className="font-smooch font-bold text-white text-8xl">POR TEMA</h2>
             </div>
           </Link>
           <Link href={{ pathname: '/empresa' }}>
-            <div className="flex min-h-screen bg-white hover:bg-black items-center justify-center">
+            <div className="flex min-h-screen bg-white hover:bg-gray-700 items-center justify-center">
               <h2 className="font-smooch font-bold text-black text-8xl">POR EMPRESA</h2>
             </div>
           </Link>
