6149a74f1f4294491bb113244d8db9b909937659
axelinstrumental <aobscura@instrumental.net>
2025-01-17 15:41:16 -0600

    implementation-for-edit

--- src/app/categorias/[categoria]/page.tsx
+++ src/app/categorias/[categoria]/page.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 import { useParams, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import { useCapitulos } from '../../../../lib/swr-hooks'
-import Logo from '../../components/Logo'
+import Image from 'next/image'
 import Search from '@/app/components/Search'
 import Loader from '@/app/components/Loader'
 
@@ -25,15 +25,20 @@ export default function Categoria() {
     
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <Logo/>
-            <p className='font-sans text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
-            <h2 className='text-4xl text-bold text-uppercase text-gray-100 font-sans font-extralight uppercase mb-5 mt-3 text-center'>{titulo}</h2>
+            <Image
+                src='/logo_seoc_entrada.png'
+                width='400'
+                height='400'
+                alt="seoc"
+            />
+            <p className='font-smooch text-4xl text-gray-700 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
+            <h2 className='font-smooch text-4xl text-bold text-uppercase text-black font-extralight uppercase mb-5 mt-3 text-center'>{titulo}</h2>
             <Search/>
-            <h2 className='text-4xl text-bold text-uppercase text-gray-100 font-sans font-extralight uppercase mb-5 mt-3'>CAPÍTULOS</h2>
+            <h2 className='font-smooch text-4xl text-bold text-uppercase text-gray-900 font-extralight uppercase mb-5 mt-3'>CAPÍTULOS</h2>
             <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-2">
                 {capitulos.map((norma: any) => (
                     <Link className='w-full' key={norma} href={`/categorias/${tit}/${norma.nombre}`}>
-                        <button className="font-sans rounded-full bg-sky-700 sm:px-10 md:px-20 py-2 mt-3 font-sans font-extralight w-full uppercase text-white" style={{'height': '90px'}}><h3 className='text-2xl'>{norma.nombre}</h3><p className='text-xxs'>{norma.descripcion}</p></button>
+                        <button className="font-smooch rounded-full bg-black sm:px-10 md:px-20 py-2 mt-3 font-extralight w-full uppercase text-white" style={{'height': '90px'}}><h3 className='text-4xl'>{norma.nombre}</h3><p className='text-2xl'>{norma.descripcion}</p></button>
                     </Link>
                 ))}
             </div>
