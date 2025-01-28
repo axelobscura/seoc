ff060eabacd683344e58006c806e5d51d273398c
axelinstrumental <aobscura@instrumental.net>
2025-01-28 14:47:26 -0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       background: 'url(bkg_entrada.jpg) no-repeat center center',
       backgroundSize: 'cover',
     }}>
-      <div className='flex items-center flex-col login' style={{
+      <div className='flex items-center flex-col login bg-white bg-opacity-20 py-10 px-10' style={{
         'marginLeft': '55%'
       }}>
         <Image
@@ -22,7 +22,7 @@ export default function Home() {
           alt="seoc"
         />
         <Link href='/login'>
-          <button className="bg-black text-white rounded-full py-2 px-20 mt-5 mr-7 ml-7" type="submit">INGRESAR</button>
+          <button className="bg-black text-white rounded-full py-2 px-20 mt-5 mr-7 ml-7" type="submit">INICIAR SESIÓN</button>
         </Link>
       </div>
     </main>
