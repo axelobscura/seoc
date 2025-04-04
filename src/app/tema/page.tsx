622bb2ad3f719abefa9d8d8e39282065e66f9bdf
axelinstrumental <aobscura@instrumental.net>
2025-04-03 18:25:59 -0600

    search

--- src/app/tema/page.tsx
+++ src/app/tema/page.tsx
@@ -49,15 +49,15 @@ export default function Tema() {
                 />
             </Link>
             <p className='font-smooch text-smooch text-sm md:text-2xl text-gray-500 font-bold'><small>Supervisor Especializado en Obras de Concreto</small></p>
-            <h2 className='font-smooch text-6xl text-gray-950 mb-2'>APRENDE O REFUERZA LO APRENDIDO</h2>
+            <h2 className='font-smooch text-6xl text-gray-950 mb-2 border-b-2 border-gray-900 pb-3'>APRENDE O REFUERZA LO APRENDIDO</h2>
             <Link href={`/secciones`}>
-                <h3 className='font-smooch text-2xl text-gray-950 bg-white py-3 px-10 md:px-36 mb-2 rounded-full shadow-lg'>SELECCIONA UN TEMA</h3>
+                <h3 className='font-smooch text-2xl text-gray-950 bg-white py-3 px-10 md:px-36 mb-2 rounded-full shadow-lg mt-3'>SELECCIONA UN TEMA</h3>
             </Link>
-            <Search/>
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-5">
+            {/* <Search/> */}
+            <div className="grid grid-cols-1 sm:grid-cols-5 gap-4 mt-5">
                 {posts.map((val: any) => (
                     <Link key={val[0]} href={`/categorias/${val[1]}?id=${val[0]}`}>
-                        <button className="font-smooch text-2xl rounded-full bg-black px-5 py-2 mt-1 w-full uppercase text-white hover:bg-gray-800 border border-gray-800" style={{
+                        <button className="font-smooch text-1xl rounded-full bg-black px-5 py-2 mt-1 w-full uppercase text-white hover:bg-gray-800 border border-gray-800" style={{
                             boxShadow: '0 0 3px 3px rgba(0, 0, 0, 0.7)',
                         }}>{val[2]}</button>
                     </Link>
